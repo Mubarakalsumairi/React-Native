1867bd103f2294e6480ddc35c33081f3df9492be
mubarakalsumairi <mubarakalsumairi@gmail.com>
2025-12-20 22:38:42 +0300

    shop4

--- app/index.tsx
+++ app/index.tsx
@@ -20,12 +20,14 @@ export default function Index() {
       />
 
       <View style={{flexDirection:"row"}} >
-        <ProductItem 
+        <ProductItem
+        key="Chocolate red"
           name="Chocolate red"
           price={3.500}
           image="https://tse3.mm.bing.net/th/id/OIP.ooZ4roMGxE241_h73fAifQHaDt?cb=ucfimg2&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3"
         />
         <ProductItem 
+        key="chocolate cake"
           name="chocolate cake"
           price={4.500}
           image="https://butternutbakeryblog.com/wp-content/uploads/2023/04/chocolate-cake.jpg"
--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
 
-type ProductItemProps = {
+interface ProductItemProps  {
     name : string;
     price : number;
     image: string;
